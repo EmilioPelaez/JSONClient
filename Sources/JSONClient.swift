00574d93b117b0189d2376b07cc505169c52b1db
Emilio Peláez <i.am@emiliopelaez.me>
2017-08-26 12:30:09 -0500

    Updated for Vapor 2

--- Sources/JSONClient.swift
+++ Sources/JSONClient.swift
@@ -13,7 +13,7 @@ import HTTP
 
 open class JSONClient {
 	public let baseUrl: String
-	public let client: ClientProtocol.Type
+	public let client: Responder
 	
 	public enum Error: Swift.Error {
 		case invalidResponse
@@ -22,7 +22,7 @@ open class JSONClient {
 	
 	open func headers() -> [HeaderKey: String] { return [:] }
 	
-	public init(baseUrl: String, client: ClientProtocol.Type) {
+	public init(baseUrl: String, client: Responder) {
 		self.baseUrl = baseUrl
 		self.client = client
 	}
@@ -32,16 +32,22 @@ open class JSONClient {
 	                         query: [String: CustomStringConvertible] = [:],
 	                         headers: [HeaderKey: String] = [:],
 	                         body: ContentBody = .empty) throws -> JSON {
-		guard let url = ([baseUrl] + components).joined(separator: "/").addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+		
+		guard let uri = ([baseUrl] + components).joined(separator: "/").addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
 			throw Error.invalidURL
 		}
+		
 		var requestHeaders = self.headers()
 		headers.forEach { requestHeaders[$0.key] = $0.value }
 		requestHeaders["Content-Type"] = body.contentType.description
 		
-		let response = try client.request(method, url, headers: requestHeaders, query: query, body: body.content)
+		let request = Request(method: method, uri: uri, headers: requestHeaders, body: body.content.makeBody())
+		if !query.isEmpty {
+			request.formURLEncoded = try Node(node: query)
+		}
+		let response = try client.respond(to: request)
 		switch response.body {
-		case .data(let bytes): return try JSON(serialized: bytes)
+		case .data(let bytes): return try JSON(bytes: bytes)
 		case _: throw Error.invalidResponse
 		}
 	}
